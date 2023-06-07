3d54b4547510bb98146b83c0cef7cd89f1ca2610
Birjer Garl <birjer@monolit.stark.nu>
2023-06-07 10:26:55 +0200

    Initial uppladdning.

---
+++ BresenhamsLineAlgorithm.c
@@ -0,0 +1,10 @@
+/*#include <stdlib.h>*/
+
+
+unsigned long BresenhamsLineAlgorithm(double **line, unsigned long lineLength, unsigned int **Map2D, unsigned int resolution) {
+  double dx = line[0][lineLength - 1] - line[0][0];
+  double dy = line[1][lineLength - 1] - line[1][0];
+
+  
+  return 0; /* Antal rutor fyllda. */
+}
